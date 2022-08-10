c7518962c487413f21c3d9201f7b075e2980bb96
Rohit-Molawade <rohit.molawade27@gmail.com>
2022-08-10 18:07:59 +0530

    created logic for marking cells

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let start = document.getElementById("start");
+
 let gameboard = (function () {
   let _game_board = [
     ["x1", "x2", "x3"],
@@ -9,49 +11,96 @@ let gameboard = (function () {
   function generate_board() {
     _game_board.forEach((array) => {
       array.forEach((cell) => {
-        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         let div = document.createElement("div");
 
         div.setAttribute("class", "cell");
         div.dataset.cell = cell;
-        div.appendChild(svg);
-        cell = div;
+        div.dataset.checked = "false";
         _gameboard_container.appendChild(div);
       });
     });
   }
 
-  /*let cells = document.querySelectorAll(".cell");
-    cells.forEach((cell) => {
-      cell.addEventListener("click", () => {
-        let svg = cell.firstChild;
-        let line1 = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "line"
-          );
-          line1.setAttribute("x1", 10);
-          line1.setAttribute("y1", 10);
-          line1.setAttribute("x2", 100);
-          line1.setAttribute("y2", 100);
-          line1.setAttribute("stroke", "white");
-          line1.setAttribute("stroke-width", 2);
-          svg.appendChild(line1);
-        if (player == "X") {
-          let line2 = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "line"
-          );
-          line2.setAttribute("x1", 10);
-          line2.setAttribute("y1", 10);
-          line2.setAttribute("x2", 100);
-          line2.setAttribute("y2", 100);
-          line2.setAttribute("stroke", "white");
-          line2.setAttribute("stroke-width", 2);
-          svg.appendChild(line2);
-        } else if (player === "O") {
-        }
-      });*/
-  return { generate_board };
+  function getindex(x) {
+    for (let i = 0; i < _game_board.length; i++) {
+      let index = _game_board[i].indexOf(x);
+      if (index > -1) return [i,index];
+    };
+  }
+
+  function _markX(e,div) {
+    let span = document.createElement("span");
+    let array = getindex(div.dataset.cell);
+    span.textContent = "X";
+    span.className = "X";
+    div.appendChild(span);
+    div.dataset.checked = "true";
+    _game_board[array[0]][array[1]] = 'X';
+  }
+
+  function _markO(e,div) {
+    let span = document.createElement("span");
+    let array = getindex(div.dataset.cell);
+    span.textContent = "O";
+    span.className = "O";
+    div.appendChild(span);
+    div.dataset.checked = "true";
+    _game_board[array[0]][array[1]] = 'O';
+  }
+
+  function select(e, chance) {
+    let div = e.target;
+    if (e.target.dataset.checked === "false") {
+      if (chance === "X") _markX(e.target,div);
+      else if (chance === "O") _markO(e.target,div);
+
+      display_controller.toggle_chance();
+    } else {
+      alert("Already checked");
+    }
+  }
+
+  return { generate_board, select };
 })();
 
+let player = function () {
+  let _name, _symbol;
+  function set_info(n, s) {
+    _name = n;
+    _symbol = s;
+  }
+  return { set_info };
+};
+
+let player1 = player;
+let player2 = player;
+
+let display_controller = (function () {
+  let _chance = "X";
+
+  function get_chance() {
+    return _chance;
+  }
+
+  function toggle_chance() {
+    if (_chance === "X") _chance = "O";
+    else if (_chance === "O") _chance = "X";
+  }
+  return { get_chance, toggle_chance };
+})();
+
+start.addEventListener("click", () => {
+  player1.set_info("Player1", "X");
+  player2.set_info("Player2", "O");
+});
+
 gameboard.generate_board();
+
+let board = document.getElementsByClassName("cell"); //Get a NodeList of all cells
+let board_array = Array.from(board); //Convert Node list to Array
+
+board_array.forEach((cell) => {
+  cell.addEventListener("click", (e) => {
+    gameboard.select(e, display_controller.get_chance());
+  });
+});
