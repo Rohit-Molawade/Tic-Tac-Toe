75eed4864d4ca7f5890eb24ea176330333b516d8
Rohit-Molawade <rohit.molawade27@gmail.com>
2022-08-11 23:10:34 +0530

    Added function to check for winner

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let start = document.getElementById("start");
 
+////////////////////////////////Game Board Module\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 let gameboard = (function () {
   let _game_board = [
     ["x1", "x2", "x3"],
@@ -7,6 +8,7 @@ let gameboard = (function () {
     ["z1", "z2", "z3"],
   ];
   let _gameboard_container = document.getElementById("board");
+  let _count = 0;
 
   function generate_board() {
     _game_board.forEach((array) => {
@@ -21,38 +23,29 @@ let gameboard = (function () {
     });
   }
 
-  function getindex(x) {
+  function _getindex(x) {
     for (let i = 0; i < _game_board.length; i++) {
       let index = _game_board[i].indexOf(x);
-      if (index > -1) return [i,index];
-    };
+      if (index > -1) return [i, index];
+    }
   }
 
-  function _markX(e,div) {
+  function _mark(div, symbol) {
     let span = document.createElement("span");
-    let array = getindex(div.dataset.cell);
-    span.textContent = "X";
-    span.className = "X";
+    let array = _getindex(div.dataset.cell);
+    span.textContent = symbol;
+    span.className = symbol;
     div.appendChild(span);
     div.dataset.checked = "true";
-    _game_board[array[0]][array[1]] = 'X';
-  }
-
-  function _markO(e,div) {
-    let span = document.createElement("span");
-    let array = getindex(div.dataset.cell);
-    span.textContent = "O";
-    span.className = "O";
-    div.appendChild(span);
-    div.dataset.checked = "true";
-    _game_board[array[0]][array[1]] = 'O';
+    _game_board[array[0]][array[1]] = symbol;
+    check_win(symbol);
   }
 
   function select(e, chance) {
     let div = e.target;
     if (e.target.dataset.checked === "false") {
-      if (chance === "X") _markX(e.target,div);
-      else if (chance === "O") _markO(e.target,div);
+      if (chance === "X") _mark(div, "X");
+      else if (chance === "O") _mark(div, "O");
 
       display_controller.toggle_chance();
     } else {
@@ -60,9 +53,49 @@ let gameboard = (function () {
     }
   }
 
+  function check_win(symbol) {
+    _count++;
+    let _row = 0,
+      _col = 0,
+      _dr = 0,
+      _dl = 0,
+      _win = 3;
+    for (let i = 0; i < _game_board.length; i++) {
+      for (let j = 0; j < _game_board[i].length; j++) {
+        _game_board[j][i] === symbol ? _col++ : (_col = 0); //Check in row
+        _game_board[i][j] === symbol ? _row++ : (_row = 0); //Check in col
+
+        //Check diagonally
+        if (_game_board[i][j] === symbol && i < _game_board.length - _win + 1) {
+          _dr = 0;
+          _dl = 0;
+          for (let z = 0; z < _win; z++) {
+            _game_board[i + z][j + z] === symbol ? _dr++ : (_dr = 0);
+            _game_board[i + z][j - z] === symbol ? _dl++ : (_dl = 0);
+          }
+        }
+      }
+      if (
+        (_col === _win || _row === _win || _dr === _win || _dl === _win) &&
+        symbol === "X"
+      ) {
+        alert("Player 1 WON!");
+        return true;
+      } else if (
+        (_col === _win || _row === _win || _dr === _win || _dl === _win) &&
+        symbol === "O"
+      ) {
+        alert("Player 2 WON!");
+        return true;
+      }
+      _row = 0;
+    }
+  }
+
   return { generate_board, select };
 })();
 
+//////////////////////////////////////Player Factory Function\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 let player = function () {
   let _name, _symbol;
   function set_info(n, s) {
@@ -72,9 +105,11 @@ let player = function () {
   return { set_info };
 };
 
+//Player Objects
 let player1 = player;
 let player2 = player;
 
+////////////////////////////////Display Module\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 let display_controller = (function () {
   let _chance = "X";
 
